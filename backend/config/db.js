de1da409a411264609e10bbfcbf0b56952d0231a
Sanket Dhuri <contact.sanketdhuri@gmail.com>
2024-05-19 13:52:05 +0530

    ercer

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://iamahacker:sanket@cluster0.b8l7zde.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/ispl', {
+        await mongoose.connect('mongodb+srv://iamahacker:sanket@cluster0.b8l7zde.mongodb.net/ispl?retryWrites=true&w=majority&appName=Cluster0', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
-        console.log('MongoDB connected');
+        console.log('MongoDB connected to ispl database');
     } catch (err) {
         console.error(err.message);
         process.exit(1);
